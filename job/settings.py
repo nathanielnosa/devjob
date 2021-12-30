7839d2039aa1ca1e95800d547339e3ceb2e3d750
nathaniel nosa <nathanielnosa@gmail.com>
2021-12-30 22:46:53 +0100

    cloudinary storages

--- job/settings.py
+++ job/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-# DEBUG_PROPAGATE_EXCEPTIONS = True
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','devjober.herokuapp.com']
 
