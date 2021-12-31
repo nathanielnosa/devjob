ded6a748647e605216304b20f0d2d8b9ab1ef0ad
nathaniel nosa <nathanielnosa@gmail.com>
2021-12-31 09:53:53 +0100

    new changes

--- job/settings.py
+++ job/settings.py
@@ -154,6 +154,8 @@ MEDIA_URL = '/img/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
