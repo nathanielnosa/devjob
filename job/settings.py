477e3ef30df2962945ed2d551a5bbfebc09fc71e
nathaniel nosa <nathanielnosa@gmail.com>
2021-12-31 11:44:43 +0100

    checking static issues

--- job/settings.py
+++ job/settings.py
@@ -150,6 +150,7 @@ STATICFILES_DIRS = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 django_heroku.settings(locals())
 
 
@@ -157,7 +158,7 @@ django_heroku.settings(locals())
 MEDIA_URL = '/img/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
 
-# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- job/urls.py
+++ job/urls.py
@@ -1,4 +1,3 @@
-from projects.views import projects
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
@@ -24,9 +23,4 @@ urlpatterns = [
 
 
 ] + static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
 urlpatterns += static (settings.STATIC_URL, document_root = settings.STATIC_ROOT) 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
