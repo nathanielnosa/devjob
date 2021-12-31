61c8c496037e9df3aba8987b705d8d810ac97141
nathaniel nosa <nathanielnosa@gmail.com>
2021-12-31 10:18:24 +0100

    checking static issues

--- job/settings.py
+++ job/settings.py
@@ -13,7 +13,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-z-yyxnwxakuyh2+qr)vxv$83wyw$c6t30s49@3nmmj@-g-20k)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
