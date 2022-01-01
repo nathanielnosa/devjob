4788bbdb8546649ee758d9cf87e8e029be94a5bf
nathaniel nosa <nathanielnosa@gmail.com>
2022-01-01 03:56:30 +0100

    static update

--- job/settings.py
+++ job/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DSECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['devjober.herokuapp.com','localhost','127.0.0.1']
 
